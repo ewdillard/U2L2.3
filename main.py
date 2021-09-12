c2f71df391ec97ce552d9b0693703761a9270198
ewdillard1 <52715741+ewdillard@users.noreply.github.com>
2021-09-12 18:59:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Date: 09/12/2021
+# Lesson: Mathmatical operands in Python
+# Operands: +. -, *, /, //, %,**  also use ()
+# Purpose: Demo program showing the use of mathmatical operands in Python
+# 1) +, - addition and subtraction
+# 2) *, / multiplication and Division
+# 3) //, floor division,  truncate any decimals.
+# 4) % modulus (think remainder in division)
+# 5) ** exponential math raising something to a power
+#------------------------------------------------------
+
+# Demon code
+# 1) Addition
+print("Addition example: 1+3 = ", 1+3)
+
+#2) Subtraction
+print("Subtracion example: 9-5 = ", 9-5)
+
+# Now you put in the rest of the examples please.
